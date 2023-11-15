fa932a4e55ffbde02845e5855bfb4840c41527f2
Zandra Nikki Godinez Tanquerido <zandranikkitanquerido@sksu.edu.ph>
2023-11-15 11:56:59 +0800

    Updating toggleTaskStatus, editTask, and deleteTask functions

--- todo-list/app.vue
+++ todo-list/app.vue
@@ -1,53 +1,50 @@
 <template>
   <div>
-
     <!-- header -->
     <div class="header">
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
       />
-
       <h1>Todo List</h1>
       <div class="create-new">
-        <input
-          v-model="newTask"
-          @keydown.enter="addTask"
-          placeholder="Add new task..."
-        />
+        <input v-model="newTask" @keydown.enter="addTask" placeholder="Add new task..." />
         <button @click="addTask"><i class="fas fa-plus"></i> Add</button>
       </div>
     </div>
 
     <!-- List of tasks container -->
-     <!-- Add the tasks container here -->
-     <div class="tasks-container">
+    <div class="tasks-container">
       <h4>Active Tasks</h4>
       <div class="tasks">
-        <div v-for="(task, index) in tasks" :key="index" :class="{ 'task': true, 'is-complete': task.completed }">
+        <div
+          v-for="(task, index) in tasks"
+          :key="index"
+          :class="{ 'task': true, 'is-complete': task.completed }"
+        >
           <div class="content" :class="{ 'completed': task.completed }">
-            <p class="{ 'text-decoration': task.completed ? 'line-through' : 'none' }">{{ task.text }}</p>
+            <p :style="{ 'text-decoration': task.completed ? 'line-through' : 'none' }">{{ task.text }}</p>
           </div>
           <div class="buttons">
-            <button @click="toggleTaskStatus(task.id)">Done</button>
-            <button @click="editTask(task.id)"><i class="fa fa-edit"></i></button>
-            <button class="delete" @click="deleteTask(task.id)"><i class="fas fa-trash-alt"></i></button>
+            <button @click="toggleTaskStatus(task.id, index)">Done</button>
+            <button @click="editTask(task.id, index)"><i class="fa fa-edit"></i></button>
+            <button class="delete" @click="deleteTask(task.id, index)"><i class="fas fa-trash-alt"></i></button>
           </div>
         </div>
       </div>
     </div>
-     <!-- Display completed tasks -->
-     <div class="completed-tasks">
+
+    <!-- Display completed tasks -->
+    <div class="completed-tasks">
       <h4>Completed Tasks</h4>
       <div v-for="(completedTask, index) in completedTasks" :key="index" class="btnCompleteTask">
-        <p> <i class="fas fa-check"></i>   {{ completedTask.text }}</p>
+        <p> <i class="fas fa-check"></i> {{ completedTask.text }}</p>
         <div>
           <button class="undo" @click="undoTask(completedTask.id)"> <i class="fas fa-undo"></i> </button>
           <button class="delete" @click="deleteCompletedTask(completedTask.id)"><i class="fas fa-trash-alt"></i></button>
         </div>
       </div>
     </div>
-   
   </div>
 </template>
 
@@ -55,15 +52,23 @@
 import { ref } from 'vue';
 import api from './api/api';
 
+let nextTaskId = 1;
+
 type Task = { id: number; text: string; completed: boolean };
 
 const newTask = ref('');
 const tasks = ref<Task[]>([]);
 const completedTasks = ref<Task[]>([]);
 
+const generateTaskId = () => {
+  return nextTaskId++;
+};
+
 const addTask = async () => {
   if (newTask.value.trim() !== '') {
+    const taskId = generateTaskId();
     const response = await api.post('/todos', {
+      id: taskId,
       text: newTask.value,
       completed: false,
     });
@@ -72,49 +77,66 @@ const addTask = async () => {
   }
 };
 
-const toggleTaskStatus = async (id: number) => {
-  const taskIndex = tasks.value.findIndex((task) => task.id === id);
-  const task = tasks.value[taskIndex];
-  if (!task.completed) {
-    await api.put(`/todos/${id}`, { ...task, completed: true });
-    task.completed = true;
-    completedTasks.value.push(task);
-    tasks.value.splice(taskIndex, 1);
+const toggleTaskStatus = async (id: number, index: number) => {
+  const task = tasks.value[index];
+  try {
+    const response = await api.patch(`/todos/${id}`, { completed: !task.completed });
+
+    tasks.value.splice(index, 1);
+    if (!task.completed) {
+      completedTasks.value.push(response.data);
+    } else {
+      tasks.value.push(response.data);
+      completedTasks.value = completedTasks.value.filter((completedTask) => completedTask.id !== id);
+    }
+  } catch (error) {
+    console.error('Error toggling task status:', error);
   }
 };
 
 const undoTask = async (id: number) => {
-  const taskIndex = completedTasks.value.findIndex((task) => task.id === id);
-  const task = completedTasks.value[taskIndex];
-  if (task) {
+  try {
+    const taskIndex = completedTasks.value.findIndex((task) => task.id === id);
+    const task = completedTasks.value[taskIndex];
     await api.put(`/todos/${id}`, { ...task, completed: false });
     completedTasks.value.splice(taskIndex, 1);
     task.completed = false;
     tasks.value.push(task);
+  } catch (error) {
+    console.error('Error undoing task:', error);
   }
 };
 
-const deleteTask = async (id: number) => {
-  await api.delete(`/todos/${id}`);
-  tasks.value = tasks.value.filter((task) => task.id !== id);
+const deleteTask = async (id: number, index: number) => {
+  try {
+    await api.delete(`/todos/${id}`);
+    tasks.value.splice(index, 1);
+  } catch (error) {
+    console.error('Error deleting task:', error);
+  }
 };
 
-const deleteCompletedTask = async (id: number) => {
-  await api.delete(`/todos/${id}`);
-  completedTasks.value = completedTasks.value.filter((task) => task.id !== id);
+const deleteCompletedTask = async (id: number, index: number) => {
+  try {
+    await api.delete(`/todos/${id}`);
+    completedTasks.value.splice(index, 1);
+  } catch (error) {
+    console.error('Error deleting completed task:', error);
+  }
 };
 
-const editTask = async (id: number) => {
-  const taskIndex = tasks.value.findIndex((task) => task.id === id);
-  const editedText = prompt('Edit task:', tasks.value[taskIndex].text);
+const editTask = async (id: number, index: number) => {
+  const editedText = prompt('Edit task:', tasks.value[index].text);
   if (editedText !== null) {
-    await api.put(`/todos/${id}`, { ...tasks.value[taskIndex], text: editedText });
-    tasks.value[taskIndex].text = editedText;
+    try {
+      await api.put(`/todos/${id}`, { ...tasks.value[index], text: editedText });
+      tasks.value[index].text = editedText;
+    } catch (error) {
+      console.error('Error editing task:', error);
+    }
   }
 };
 </script>
-
-
 
 <style scoped>
   @import './assets/main.css';
