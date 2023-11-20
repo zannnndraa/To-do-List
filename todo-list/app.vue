720b79747ab724d1ff3b66a1ca2738c4d806c62e
Zandra Nikki Godinez Tanquerido <zandranikkitanquerido@sksu.edu.ph>
2023-11-20 11:25:53 +0800

    updating app using fetch of nuxt js

--- todo-list/app.vue
+++ todo-list/app.vue
@@ -1,172 +1,172 @@
-  <template>
-    <div>
-      <!-- header -->
-      <div class="header">
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-        />
-        <h1>Todo List</h1>
-        <div class="create-new">
-          <input v-model="newTask" @keydown.enter="addTask" placeholder="Add new task..." />
-          <button @click="addTask"><i class="fas fa-plus"></i> Add</button>
-
-        </div>
+<!-- app.vue -->
+<template>
+  <div>
+    <!-- header -->
+    <div class="header">
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+      />
+      <h1>Todo List</h1>
+      <div class="create-new">
+        <input v-model="newTask" @keydown.enter="addTask" placeholder="Add new task..." />
+        <button @click="addTask"><i class="fas fa-plus"></i> Add</button>
       </div>
+    </div>
 
-      <!-- List of tasks -->
+    <!-- List of tasks -->
       <div class="tasks-container">
-    <h4>Active Tasks</h4>
-    <div class="tasks">
-      <div
-        v-for="(task, index) in tasks"
-        :key="index"
-        :class="{ 'task': true, 'is-complete': task.completed }"
-      >
-        <div class="content" :class="{ 'completed': task.completed }">
-          <p :style="{ 'text-decoration': task.completed ? 'line-through' : 'none' }">{{ task.text }}</p>
-          <p class="task-date">{{ task.date }}</p> 
-        </div>
-        
-            <div class="buttons">
-              <button @click="toggleTaskStatus(task.id, index)">Done</button>
-              <button @click="editTask(task.id, index)"><i class="fa fa-edit"></i></button>
-              <button class="delete" @click="deleteTask(task.id, index)"><i class="fas fa-trash-alt"></i></button>
-            </div>
-          </div>
-        </div>
-      </div>
+  <h4>Active Tasks</h4>
+  <div class="tasks">
+    <div v-for="(task, index) in tasks" :key="index">
+      <div class="content" :class="{ 'completed': task.completed }">
+        <p v-if="task && task.completed !== undefined" :style="{ 'text-decoration': task && task.completed ? 'line-through' : 'none' }">{{ task && task.text }}</p>
 
-      <!-- completed tasks -->
-      <div class="completed-tasks">
-        <h4>Completed Tasks</h4>
-        <div v-for="(completedTask, index) in completedTasks" :key="index" class="btnCompleteTask">
-          <p> <i class="fas fa-check"></i> {{ completedTask.text }}</p>
-          <div>
-            <button class="undo" @click="undoTask(completedTask.id, index)"> <i class="fas fa-undo"></i> </button>
-            <button class="delete" @click="deleteCompletedTask(completedTask.id, index)"><i class="fas fa-trash-alt"></i></button>
+        <p class="task-date">{{ task.date }}</p>
+          </div>
+          <div class="buttons">
+            <button @click="toggleTaskStatus(task.id, index)">Done</button>
+            <button @click="editTask(task.id, index)"><i class="fa fa-edit"></i></button>
+            <button class="delete" @click="deleteTask(task.id, index)"><i class="fas fa-trash-alt"></i></button>
           </div>
         </div>
       </div>
     </div>
-  </template>
 
-  <script setup lang="ts">
-  import { ref } from 'vue';
-  import api from './server/todos';
+    <!-- completed tasks -->
+    <div class="completed-tasks">
+      <h4>Completed Tasks</h4>
+      <div v-for="(completedTask, index) in completedTasks" :key="index" class="btnCompleteTask">
+        <p> <i class="fas fa-check"></i> {{ completedTask.text }}</p>
+        <div>
+          <button class="undo" @click="undoTask(completedTask.id, index)"> <i class="fas fa-undo"></i> </button>
+          <button class="delete" @click="deleteCompletedTask(completedTask.id, index)"><i class="fas fa-trash-alt"></i></button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-  let nextTaskId = 1;
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import api from './server/todos.js';
 
-  type Task = { id: number; text: string; completed: boolean; date: string };
+let nextTaskId = 0;
+
+type Task = { id: number; text: string; completed: boolean; date: string };
+
+const newTask = ref('');
+const tasks = ref<Task[]>([]);
+const completedTasks = ref<Task[]>([]);
+
+const generateTaskId = () => {
+  const currentDate = new Date();
+  const dateString = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
+  return ++nextTaskId;
+  
+};
 
 
-  const newTask = ref('');
-  const tasks = ref<Task[]>([]);
-  const completedTasks = ref<Task[]>([]);
-
-  const generateTaskId = () => {
-    const currentDate = new Date();
-    const dateString = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
-    return `${dateString}-${nextTaskId++}`;
-  };
-
-
-  const addTask = async () => {
-    
+const addTask = async () => {
   if (newTask.value.trim() !== '') {
     const taskId = generateTaskId();
-    const currentDate = new Date();
-    const date = `${currentDate.toLocaleDateString()} ${currentDate.toLocaleTimeString()}`;
 
     try {
       const response = await api.post('/todos', {
         id: taskId,
         text: newTask.value,
         completed: false,
-        date: date,
+        date: new Date().toISOString(),
       });
 
-      const addedTask = response;
+      const addedTask = response.data;
 
-      tasks.value.push(addedTask);
-      newTask.value = '';
+      
+      if (addedTask && addedTask.id && addedTask.text && addedTask.date) {
+        tasks.value.push(addedTask);
+        newTask.value = '';
+        console.log('Task added successfully:', tasks);
+      } else {
+        console.error('Invalid response format for added task:', tasks);
+      }
+
     } catch (error) {
       console.error('Error adding task:', error);
     }
   }
 };
 
-  const toggleTaskStatus = async (id: number, index: number) => {
-    const task = tasks.value[index];
-    try {
-      const response = await api.patch(`/todos/${id}`, { completed: !task.completed });
 
-      tasks.value.splice(index, 1);
-      if (!task.completed) {
-        const completedTask = response.data;
-        completedTasks.value.push(completedTask);
-      } else {
-        completedTasks.value = completedTasks.value.filter(
-          (completedTask) => completedTask.id !== id
-        );
-        tasks.value.push(response.data);
-      }
-    } catch (error) {
-      console.error('Error toggling task status:', error);
-    }
-  };
-  const undoTask = async (id: number, index: number) => {
-    const task = completedTasks.value[index];
-    if (task) {
-      try {
-        const response = await api.patch(`/todos/${id}`, { completed: false });
-        completedTasks.value.splice(index, 1);
-        task.completed = false;
-        tasks.value.push(task);
-      } catch (error) {
-        console.error('Error undoing task:', error);
-      }
-    }
-  };
+const toggleTaskStatus = async (id: number, index: number) => {
+  const task = tasks.value[index];
+  try {
+    const response = await api.patch(`/todos/${id}`, { completed: !task.completed });
 
-  const deleteTask = async (id: number, index: number) => {
-    try {
-      await api.delete(`/todos/${id}`);
-      tasks.value.splice(index, 1);
-    } catch (error) {
-      console.error('Error deleting task:', error);
+    tasks.value.splice(index, 1);
+    if (!task.completed) {
+      const completedTask = response.data;
+      completedTasks.value.push(completedTask);
+    } else {
+      completedTasks.value = completedTasks.value.filter(
+        (completedTask) => completedTask.id !== id
+      );
+      tasks.value.push(response.data);
     }
-  };
+  } catch (error) {
+    console.error('Error toggling task status:', error);
+  }
+};
 
-  const deleteCompletedTask = async (id: number, index: number) => {
+const undoTask = async (id: number, index: number) => {
+  const task = completedTasks.value[index];
+  if (task) {
     try {
-      await api.delete(`/todos/${id}`);
+      const response = await api.patch(`/todos/${id}`, { completed: false });
       completedTasks.value.splice(index, 1);
+      task.completed = false;
+      tasks.value.push(task);
     } catch (error) {
-      console.error('Error deleting completed task:', error);
+      console.error('Error undoing task:', error);
     }
-  };
+  }
+};
 
-  const editTask = async (id: number, index: number) => {
-    const editedText = prompt('Edit task:', tasks.value[index].text);
-    if (editedText !== null) {
-      try {
-        const response = await api.patch(`/todos/${id}`, { ...tasks.value[index], text: editedText });
-        tasks.value[index] = response.data; 
-      } catch (error) {
-        console.error('Error editing task:', error);
-      }
+const deleteTask = async (id: number, index: number) => {
+  try {
+    await api.delete(`/todos/${id}`);
+    tasks.value.splice(index, 1);
+  } catch (error) {
+    console.error('Error deleting task:', error);
+  }
+};
+
+const deleteCompletedTask = async (id: number, index: number) => {
+  try {
+    await api.delete(`/todos/${id}`);
+    completedTasks.value.splice(index, 1);
+  } catch (error) {
+    console.error('Error deleting completed task:', error);
+  }
+};
+
+const editTask = async (id: number, index: number) => {
+  const editedText = prompt('Edit task:', tasks.value[index].text);
+  if (editedText !== null) {
+    try {
+      const response = await api.patch(`/todos/${id}`, { ...tasks.value[index], text: editedText });
+      tasks.value[index] = response.data;
+    } catch (error) {
+      console.error('Error editing task:', error);
     }
-  };
+  }
+};
+</script>
 
+<style scoped>
+  @import './assets/main.css';
 
-  </script>
-
-  <style scoped>
-    @import './assets/main.css';
-
-    ::placeholder {
-      color: rgb(119, 170, 151);
-      font-size: 13px;
-    }
-  </style>
+  ::placeholder {
+    color: rgb(119, 170, 151);
+    font-size: 13px;
+  }
+</style>
--- todo-list/server/todos.js
+++ todo-list/server/todos.js
@@ -1,40 +1,64 @@
-const BASE_URL = 'http://localhost:3001';
+// server/todos.js
 
+const BASE_URL = 'http://localhost:3000';
+
+const useFetch = async (url, options) => {
+  const response = await fetch(url, options);
+  return response;
+};
+
+const handleResponse = async (response) => {
+  console.log('Response status:', response.status);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const contentType = response.headers.get('content-type');
+  if (contentType && contentType.includes('application/json')) {
+    const jsonData = await response.json();
+    return jsonData;
+  } else {
+    console.warn('Response is not JSON:', response);
+    return response.text();
+  }
+};
 
 const api = {
   async get(endpoint) {
-    const response = await fetch(`${BASE_URL}${endpoint}`);
+    const response = await useFetch(`${BASE_URL}${endpoint}`, {
+      method: 'GET',
+    });
     return handleResponse(response);
   },
-
   async post(endpoint, data) {
-    const response = await fetch(`${BASE_URL}${endpoint}`, {
+    const response = await useFetch(`${BASE_URL}${endpoint}`, {
       method: 'POST',
+      body: JSON.stringify(data),
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data), 
     });
     return handleResponse(response);
   },
 
   async patch(endpoint, data) {
-    const response = await fetch(`${BASE_URL}${endpoint}`, {
+    const response = await useFetch(`${BASE_URL}${endpoint}`, {
       method: 'PATCH',
+      body: JSON.stringify(data),
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data),
     });
     return handleResponse(response);
   },
 
   async delete(endpoint) {
-    const response = await fetch(`${BASE_URL}${endpoint}`, {
+    const response = await useFetch(`${BASE_URL}${endpoint}`, {
       method: 'DELETE',
     });
     return handleResponse(response);
   },
 };
 
-export default api; 
+export default api;
